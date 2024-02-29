dedd3f9e74e0436e037ce61bab167d55affb0730
ethas <liebeedelvais@gmail.com>
2024-02-29 13:29:33 +0700

    Update for User manage

--- src/pages/admin/ManageUser.jsx
+++ src/pages/admin/ManageUser.jsx
@@ -1,221 +1,182 @@
-import React, { useState } from 'react'
+import React, { Component } from 'react';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faSearch,
-  faPlus,
-  faTrashCan,
-  faPencil,
-  faUser,
-  faRepeat
-} from "@fortawesome/free-solid-svg-icons";
+import { faSearch, faSort, faCheck, faBan, faTrashAlt } from "@fortawesome/free-solid-svg-icons"; // Import faSearch icon
+import axios from 'axios';
+import {toast} from "react-toastify";
 
-const Icon = ({ classIcon, color, size }) => {
-  const iconSize = {
-    width: size,
-    height: size,
-    color: color,
-    cursor: "pointer"
+class ManageUser extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: [],
+      searchValue: '', // Add searchValue state
+      sortBy: '', // Add sortBy state
+      filterBy: 'all', // Add filterBy state with default value 'all'
+    };
+  }
+
+  componentDidMount() {
+    this.fetchUsers();
+  }
+
+  fetchUsers = async () => {
+    try {
+      const response = await axios.get('http://localhost:8080/users');
+      const users = response.data._embedded.users;
+      this.setState({ users });
+    } catch (error) {
+      console.error('Error fetching users:', error);
+    }
   };
 
-  return (
-    <span>
-      <FontAwesomeIcon icon={classIcon} style={iconSize} />
-    </span>
-
-  );
-};
-
-const ManageUser = () => {
-  const [isModeShow, setModeShow] = useState(false);
-
-  const handleModeShowToggle = () => {
-    setModeShow(!isModeShow);
+  handleSearchChange = (e) => {
+    this.setState({ searchValue: e.target.value }); // Update searchValue state when input changes
   };
 
-  return (
-    <div className='h-auto pl-3'>
-      <div className='w-full h-[150px] relative'>
-        <div className='title-admin absolute top-0 left-0'>QUẢN LÝ NGƯỜI DÙNG</div>
-        <div className='absolute bottom-0 left-0'>
-          <input
-            className='bg-[#EAEDF2] border-2 border-[#858585] rounded-[5px] w-[400px] h-[40px] px-2'
-            placeholder='Nhập tên, email, số điện thoại...'
-          />
-        </div>
-        <div className='absolute bottom-1 left-[365px]'><Icon classIcon={faSearch} color={"black"} size={"24px"} /></div>
-        <div className='absolute z-10 top-0 right-[190px] w-[80px] h-[80px] bg-[#60B664] text-[24px] font-bold text-white text-center flex flex-col justify-center rounded-[10px] shadow1'>
-          <Icon classIcon={faUser} color={"white"} size={"32px"} />
-        </div>
-        <div className='absolute top-[40px] right-0 w-[300px] h-[110px] rounded-[5px] bg-white shadow1 flex flex-col justify-center text-right px-[30px]'>
-          <div>
-            <div className='text-[24px] text-black'>56</div>
-            <div className='text-[24px] text-black'>Nhân viên</div>
-          </div>
-        </div>
+  handleToggleUserStatus = async (userId, currentStatus) => {
+    try {
+      if (currentStatus === 'enabled') {
+        await axios.put(`http://localhost:8080/user/${userId}/disable`);
+      } else {
+        await axios.put(`http://localhost:8080/user/${userId}/enable`);
+      }
+      // After successfully toggling status, fetch users again to update the user list
+      await this.fetchUsers();
+    } catch (error) {
+      console.error('Error toggling user status:', error);
+    }
+  };
 
-        <div className='absolute z-10 top-0 right-[540px] w-[80px] h-[80px] bg-[#348EED] text-[24px] font-bold text-white text-center flex flex-col justify-center rounded-[10px] shadow1'>
-          <Icon classIcon={faUser} color={"white"} size={"32px"} />
-        </div>
-        <div className='absolute top-[40px] right-[350px] w-[300px] h-[110px] rounded-[5px] bg-white shadow1 flex flex-col justify-center text-right px-[30px]'>
-          <div>
-            <div className='text-[24px] text-black'>1254</div>
-            <div className='text-[24px] text-black'>Khách hàng</div>
+  handleDeleteUser = async (userId) => {
+    try {
+      const url = `http://localhost:8080/users/${userId}`;
+      const response = await fetch(url, {
+            method: 'DELETE'
+          }
+      )
+      if (response.ok) {
+        // setIsChanged(!isChanged);
+        // setCurrentPage(1);
+        await this.fetchUsers();
+        toast.success("Xóa người dùng thành công");
+      } else {
+        toast.warning("Đã xảy ra lỗi trong quá trình xóa người dùng!");
+      }
+    }catch (error){
+      {
+        toast.error("Đã xảy ra lỗi trong quá trình xóa người dùng!");
+      }
+    }
+  };
+
+  handleDeleteUserConfirmation = (userId) => {
+    if (window.confirm('Bạn có chắc chắn muốn xóa người dùng này?')) {
+      this.handleDeleteUser(userId);
+    }
+  };
+
+  handleSortChange = (sortBy) => {
+    this.setState({ sortBy });
+  };
+
+  handleFilterChange = (filterBy) => {
+    this.setState({ filterBy });
+  };
+
+  render() {
+    const { users, searchValue, filterBy } = this.state;
+
+    // Filter users based on searchValue
+    let filteredUsers = users.filter(user =>
+        user.firstName.toLowerCase().includes(searchValue.toLowerCase()) ||
+        user.lastName.toLowerCase().includes(searchValue.toLowerCase()) ||
+        user.username.toLowerCase().includes((searchValue.toLowerCase())) ||
+        user.phonenumber.includes(searchValue)
+    );
+
+    // Apply filter based on filterBy state
+    if (filterBy === 'active') {
+      filteredUsers = filteredUsers.filter(user => user.enabled);
+    } else if (filterBy === 'disabled') {
+      filteredUsers = filteredUsers.filter(user => !user.enabled);
+    }
+
+    return (
+        <div className='h-auto pl-3'>
+          <div className='w-full h-[150px] relative'>
+            <div className='title-admin absolute top-0 left-0'>QUẢN LÝ NGƯỜI DÙNG</div>
+            <div className='absolute bottom-0 left-0'>
+              <input
+                  className='bg-[#EAEDF2] border-2 border-[#858585] rounded-[5px] w-[400px] h-[40px] px-2'
+                  placeholder='Nhập họ, tên, username hoặc số điện thoạt....'
+                  value={searchValue} // Bind input value to searchValue state
+                  onChange={this.handleSearchChange} // Add onChange event handler
+              />
+            </div>
+
+            <div>
+              <div className='absolute bottom-1 left-[365px]'>
+                <FontAwesomeIcon icon={faSearch} color={"black"} size={"24px"} />
+              </div>
+              <div className='absolute bottom-1 right-[20vw] '>
+                <select value={filterBy} onChange={(e) => this.handleFilterChange(e.target.value)}>
+                  <option value="all">Tất cả</option>
+                  <option value="active">Hoạt động</option>
+                  <option value="disabled">Vô hiệu hoá</option>
+                </select>
+              </div>
+            </div>
+
           </div>
-        </div>
-      </div>
 
 
-      <div className='table-all-posts h-auto mt-[50px]'>
-        <div className={`w-4/5 h-[60px] shadow1 relative top-7 ${isModeShow ? 'bg-[#60B664]' : 'bg-[#348EED]'} text-center text-[24px] flex flex-col justify-center mx-auto rounded-[10px] text-white`}>
-          {isModeShow === false ? (`KHÁCH HÀNG`) : (`NHÂN VIÊN`)}
-          <div className='absolute right-[10px] w-[40px] h-[40px] border-2 border-white rounded-[5px] flex flex-col justify-center cursor-pointer' onClick={handleModeShowToggle}>
-            <Icon classIcon={faRepeat} color={"white"} size={"24px"} />
-          </div>
-          <div className='absolute right-[60px] w-[40px] h-[40px] border-2 border-white rounded-[5px] flex flex-col justify-center cursor-pointer'>
-            <Icon classIcon={faPlus} color={"white"} size={"24px"} />
-          </div>
-        </div>
-
-        {isModeShow === false ? (
-          <>
+          <div className='table-all-posts h-auto mt-[50px]'>
             <div className='h-[69vh] w-full bg-white shadow1 pt-[50px] px-[50px] rounded-[10px]'>
               <div className='grid grid-cols-10 py-3 gap-2'>
                 <div className='col-span-1 text-[#348EED]'>ID</div>
-                <div className='col-span-2 text-[#348EED]'>Họ và tên</div>
-                <div className='col-span-3 text-[#348EED]'>Địa chỉ</div>
+                <div className='col-span-1 text-[#348EED]'>Tên người dùng</div>
+                <div className='col-span-1 text-[#348EED]'>Họ và tên</div>
                 <div className='col-span-2 text-[#348EED]'>Email</div>
                 <div className='col-span-1 text-[#348EED]'>Số điện thoại</div>
-                <div className='col-span-1 text-[#348EED]'></div>
+                <div className='col-span-1 text-[#348EED]'>Trạng thái</div>
+                <div className='col-span-1 text-[#348EED]'>Hành động</div>
               </div>
 
-              <div className='grid grid-cols-10 border-t-2 border-[#D9D9D9] py-3 gap-2'>
-                <div className='col-span-1 text-black flex flex-col justify-center'>1</div>
-                <div className='col-span-2 text-black flex flex-col justify-center'>Nguyễn Công Chiến</div>
-                <div className='col-span-3 text-black flex flex-col justify-center'>Hồ Chí Minh</div>
-                <div className='col-span-2 text-black flex flex-col justify-center'>ncc@gmail.com</div>
-                <div className='col-span-1 text-black flex flex-col justify-center'>01273687123</div>
-                <div className='col-span-1 text-black flex flex-col justify-center'>
-                  <div className='flex justify-end gap-2'>
-                    <Icon classIcon={faTrashCan} color={"black"} size={"20px"} />
-                    <Icon classIcon={faPencil} color={"black"} size={"20px"} />
+              {filteredUsers.map(user => (
+                  <div key={user.userId} className='grid grid-cols-10 border-t-2 border-[#D9D9D9] py-3 gap-2'>
+                    <div className='col-span-1 text-black flex flex-col justify-center'>{user.userId}</div>
+                    <div className='col-span-1 text-black flex flex-col justify-center'>{user.username}</div>
+
+                    <div
+                        className='col-span-1 text-black flex flex-col justify-center'>{`${user.firstName} ${user.lastName}`}</div>
+                    <div className='col-span-2 text-black flex flex-col justify-center'>{user.email}</div>
+                    <div className='col-span-1 text-black flex flex-col justify-center'>{user.phonenumber}</div>
+                    <div className='col-span-1 text-black flex flex-col justify-center'>
+                      {user.enabled ? "Còn hoạt động" : "Đã vô hiệu hoá"}
+                    </div>
+
+                    <div className='col-span-1 flex justify-end items-center'>
+                      <button
+                          onClick={() => this.handleToggleUserStatus(user.userId, user.enabled ? 'enabled' : 'disabled')}
+                          style={{ backgroundColor: user.enabled ? 'green' : 'red', color: 'white', marginRight: '10px' }}
+                      >
+                        <FontAwesomeIcon icon={user.enabled ? faCheck : faBan} color={"white"} size={"20px"} />
+                      </button>
+                      <button
+                          onClick={() => this.handleDeleteUserConfirmation(user.userId)}
+                          style={{ backgroundColor: 'red', color: 'white' }}
+                      >
+                        <FontAwesomeIcon icon={faTrashAlt} color={"white"} size={"20px"} />
+                      </button>
+                    </div>
+
                   </div>
-                </div>
-              </div>
-
-              <div className='grid grid-cols-10 border-t-2 border-[#D9D9D9] py-3 gap-2'>
-                <div className='col-span-1 text-black flex flex-col justify-center'>2</div>
-                <div className='col-span-2 text-black flex flex-col justify-center'>Nguyễn Công Chiến</div>
-                <div className='col-span-3 text-black flex flex-col justify-center'>Hồ Chí Minh</div>
-                <div className='col-span-2 text-black flex flex-col justify-center'>ncc@gmail.com</div>
-                <div className='col-span-1 text-black flex flex-col justify-center'>01273687123</div>
-                <div className='col-span-1 text-black flex flex-col justify-center'>
-                  <div className='flex justify-end gap-2'>
-                    <Icon classIcon={faTrashCan} color={"black"} size={"20px"} />
-                    <Icon classIcon={faPencil} color={"black"} size={"20px"} />
-                  </div>
-                </div>
-              </div>
-
-              <div className='grid grid-cols-10 border-t-2 border-[#D9D9D9] py-3 gap-2'>
-                <div className='col-span-1 text-black flex flex-col justify-center'>3</div>
-                <div className='col-span-2 text-black flex flex-col justify-center'>Nguyễn Công Chiến</div>
-                <div className='col-span-3 text-black flex flex-col justify-center'>Hồ Chí Minh</div>
-                <div className='col-span-2 text-black flex flex-col justify-center'>ncc@gmail.com</div>
-                <div className='col-span-1 text-black flex flex-col justify-center'>01273687123</div>
-                <div className='col-span-1 text-black flex flex-col justify-center'>
-                  <div className='flex justify-end gap-2'>
-                    <Icon classIcon={faTrashCan} color={"black"} size={"20px"} />
-                    <Icon classIcon={faPencil} color={"black"} size={"20px"} />
-                  </div>
-                </div>
-              </div>
-
-              <div className='grid grid-cols-10 border-t-2 border-[#D9D9D9] py-3 gap-2'>
-                <div className='col-span-1 text-black flex flex-col justify-center'>4</div>
-                <div className='col-span-2 text-black flex flex-col justify-center'>Nguyễn Công Chiến</div>
-                <div className='col-span-3 text-black flex flex-col justify-center'>Hồ Chí Minh</div>
-                <div className='col-span-2 text-black flex flex-col justify-center'>ncc@gmail.com</div>
-                <div className='col-span-1 text-black flex flex-col justify-center'>01273687123</div>
-                <div className='col-span-1 text-black flex flex-col justify-center'>
-                  <div className='flex justify-end gap-2'>
-                    <Icon classIcon={faTrashCan} color={"black"} size={"20px"} />
-                    <Icon classIcon={faPencil} color={"black"} size={"20px"} />
-                  </div>
-                </div>
-              </div>
-
+              ))}
             </div>
-          </>
-        ) : (
-          <>
-            <div className='h-[69vh] w-full bg-white shadow1 pt-[50px] px-[50px] rounded-[10px]'>
-              <div className='grid grid-cols-7 py-3 gap-2'>
-                <div className='col-span-1 text-[#60B664]'>ID</div>
-                <div className='col-span-2 text-[#60B664]'>Họ và tên</div>
-                <div className='col-span-2 text-[#60B664]'>Email</div>
-                <div className='col-span-1 text-[#60B664]'>Số điện thoại</div>
-                <div className='col-span-1 text-[#60B664]'></div>
-              </div>
-
-              <div className='grid grid-cols-7 border-t-2 border-[#D9D9D9] py-3 gap-2'>
-                <div className='col-span-1 text-black flex flex-col justify-center'>1</div>
-                <div className='col-span-2 text-black flex flex-col justify-center'>Nguyễn Công Chiến</div>
-                <div className='col-span-2 text-black flex flex-col justify-center'>ncc@gmail.com</div>
-                <div className='col-span-1 text-black flex flex-col justify-center'>01273687123</div>
-                <div className='col-span-1 text-black flex flex-col justify-center'>
-                  <div className='flex justify-end gap-2'>
-                    <Icon classIcon={faTrashCan} color={"black"} size={"20px"} />
-                    <Icon classIcon={faPencil} color={"black"} size={"20px"} />
-                  </div>
-                </div>
-              </div>
-
-              <div className='grid grid-cols-7 border-t-2 border-[#D9D9D9] py-3 gap-2'>
-                <div className='col-span-1 text-black flex flex-col justify-center'>2</div>
-                <div className='col-span-2 text-black flex flex-col justify-center'>Nguyễn Công Chiến</div>
-                <div className='col-span-2 text-black flex flex-col justify-center'>ncc@gmail.com</div>
-                <div className='col-span-1 text-black flex flex-col justify-center'>01273687123</div>
-                <div className='col-span-1 text-black flex flex-col justify-center'>
-                  <div className='flex justify-end gap-2'>
-                    <Icon classIcon={faTrashCan} color={"black"} size={"20px"} />
-                    <Icon classIcon={faPencil} color={"black"} size={"20px"} />
-                  </div>
-                </div>
-              </div>
-
-              <div className='grid grid-cols-7 border-t-2 border-[#D9D9D9] py-3 gap-2'>
-                <div className='col-span-1 text-black flex flex-col justify-center'>3</div>
-                <div className='col-span-2 text-black flex flex-col justify-center'>Nguyễn Công Chiến</div>
-                <div className='col-span-2 text-black flex flex-col justify-center'>ncc@gmail.com</div>
-                <div className='col-span-1 text-black flex flex-col justify-center'>01273687123</div>
-                <div className='col-span-1 text-black flex flex-col justify-center'>
-                  <div className='flex justify-end gap-2'>
-                    <Icon classIcon={faTrashCan} color={"black"} size={"20px"} />
-                    <Icon classIcon={faPencil} color={"black"} size={"20px"} />
-                  </div>
-                </div>
-              </div>
-
-              <div className='grid grid-cols-7 border-t-2 border-[#D9D9D9] py-3 gap-2'>
-                <div className='col-span-1 text-black flex flex-col justify-center'>4</div>
-                <div className='col-span-2 text-black flex flex-col justify-center'>Nguyễn Công Chiến</div>
-                <div className='col-span-2 text-black flex flex-col justify-center'>ncc@gmail.com</div>
-                <div className='col-span-1 text-black flex flex-col justify-center'>01273687123</div>
-                <div className='col-span-1 text-black flex flex-col justify-center'>
-                  <div className='flex justify-end gap-2'>
-                    <Icon classIcon={faTrashCan} color={"black"} size={"20px"} />
-                    <Icon classIcon={faPencil} color={"black"} size={"20px"} />
-                  </div>
-                </div>
-              </div>
-
-            </div>
-          </>
-        )}
-
-      </div>
-    </div>
-  )
+          </div>
+        </div>
+    );
+  }
 }
 
-export default ManageUser
+export default ManageUser;
